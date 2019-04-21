eed468b3d43ef0fe9e98b1766c49e46140555924
zkc <zkyle12@gmail.com>
2019-04-21 15:18:49 -0600

    clean up, prep for get endpoint

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ app.get('/', (req, res) => {
   res.status(200).send('hello there')
 })
 
-
-// add harvest
 /*
+  POST harvest
 {
   "plantCount": 528,
   "harvestGrams": 202,
@@ -31,41 +30,20 @@ app.get('/', (req, res) => {
   "strain": "PEX"
 }
 */
-
-app.get('/test-bay', (req, res) => { 
-  knex('bays').where({id : "nw-615"})
-    .then(row => {
-      res.status(200).send({row})
-    })
-})
-
 app.post('/harvests', async (req, res) => {
-  // confirm all good
-
-  // add to DB
-  // plant_count
-  // harvest_grams
-  // total_plant_grams
-  // classification
-  // bay -fk
-  // strain -fk
-  // date 
-  // id - pk
-
   const newHarvest = {
-    ...req.body, 
+    ...req.body, // add check for all keys
     id: uuid(), 
     date: new Date().toISOString().split('T')[0], 
   }
 
-  let bay = await knex('bays').where({ id : newHarvest.bay })
-  bay = bay[0] //find one?
+  const bays = await knex('bays').where({ id : newHarvest.bay })
+  const bay = bays[0] // find one?
 
   if (!bay) {
     res.status(400).send(`bay: ${newHarvest.bay} not found`)
     return
   }
-
 
   const harvestRow = {
     id: newHarvest.id,
@@ -87,34 +65,7 @@ app.post('/harvests', async (req, res) => {
     return
   }
 
-
   // generate response
-  /*
-  {
-  "id": "uih4r9u8ewafu",
-  "plantCount": 528,
-  "harvestGrams": 202,
-  "totalPlantGrams": 3410,
-  "classification": "REC",
-  "bay": "nw-615",
-  "strain": "PEX",
-  "date": "01/01/2019",
-  
-  "totalPlantLbs": 7.6116071429, -
-  "harvestLbs": 0.4508928571, -
-
-  "percentHarvestedPlantWeight": 5.923753666,
-  "lbsHarvestedPerSqFt": 0.008318696331,
-  "plantsPerLight": 9.4285714286, --- need bay!
-  "harvestLbsPerLight": 0.008051658162,
-  "sqFtPerPlant": 0.5770491803
-  }
-
-  bay: 
-  id
-  light_count
-  square_footage 
-  */
   newHarvest.harvestLbs = convertGramsToPounds(newHarvest.harvestGrams)
   newHarvest.totalPlantLbs = convertGramsToPounds(newHarvest.harvestLbs)
   newHarvest.percentHarvestedPlantWeight = (newHarvest.harvestGrams / newHarvest.totalPlantGrams)
@@ -125,10 +76,10 @@ app.post('/harvests', async (req, res) => {
   res.status(201).send(newHarvest)
 })
 
-// get harvests
 /*
-Parameters
+  GET harvests
 
+  Parameters
     strain
     Limit response to provided strain
     Example: PEX.
@@ -149,13 +100,13 @@ Parameters
     Default: DESC.
     String
 */
-// app.get('/harvests', (req, res) => {
-//   // check query
+app.get('/harvests', (req, res) => {
+  // check query
 
-//   // get from db
+  // get from db
 
-//   // return list  
-// })
+  // return list  
+})
 
 const server = app.listen(config.get('PORT'), () => {
   const port = server.address().port
