aa8cc6738bed4235e4e5a8e98abffd9fb752ce6b
zkc <zkyle12@gmail.com>
2019-04-21 16:25:57 -0600

    finish GET, clean up

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ app.use(bodyParser.json())
 
 const knex = dbConnect()
 
-function convertGramsToPounds (grams) {
-  return grams / 453.59237
-}
-
 app.get('/', (req, res) => {
   res.status(200).send('hello there')
 })
@@ -34,7 +30,7 @@ app.post('/harvests', async (req, res) => {
   const newHarvest = {
     ...req.body, // add check for all keys
     id: uuid(), 
-    date: new Date().toISOString().split('T')[0], 
+    date: new Date().toISOString(), 
   }
 
   const bays = await knex('bays').where({ id : newHarvest.bay })
@@ -66,14 +62,9 @@ app.post('/harvests', async (req, res) => {
   }
 
   // generate response
-  newHarvest.harvestLbs = convertGramsToPounds(newHarvest.harvestGrams)
-  newHarvest.totalPlantLbs = convertGramsToPounds(newHarvest.harvestLbs)
-  newHarvest.percentHarvestedPlantWeight = (newHarvest.harvestGrams / newHarvest.totalPlantGrams)
-  newHarvest.lbsHarvestedPerSqFt = (newHarvest.harvestLbs / bay.square_footage)
-  newHarvest.harvestLbsPerLight = (newHarvest.harvestLbs / bay.light_count)
-  newHarvest.sqFtPerPlant = (bay.square_footage / newHarvest.plantCount)
+  const fullHarvest = await expandHarvest(newHarvest)
 
-  res.status(201).send(newHarvest)
+  res.status(201).send(fullHarvest)
 })
 
 /*
@@ -99,15 +90,85 @@ app.post('/harvests', async (req, res) => {
     ASC or DESC
     Default: DESC.
     String
+
+    /harvests?strain=PEX&bay=nw615&sort_by=date&order=DESC
 */
-app.get('/harvests', (req, res) => {
+
+const validSortColumns = ['date', 'harvestLbs', 'percentHarvestedPlantWeight', 'lbsHarvestedPerSqFt', 'sqFtPerPlant']
+app.get('/harvests', async (req, res) => {
   // check query
+  const filter = {}
+  if (!!req.query.strain) {
+    filter.strain = req.query.strain 
+  }
 
-  // get from db
+  if (!!req.query.bay) {
+    filter.bay = bay
+  } 
 
-  // return list  
+  const sortColumn = (!!req.query.sort_by && validSortColumns.includes(req.query.sort_by)) ? req.query.sort_by : 'date'
+  const order = !!req.query.order ? req.query.order : 'DESC'
+
+
+  let harvests
+  try {
+    harvests = await knex('harvests').where(filter)
+  } catch (e) {
+    res.status(500).send('DB error')
+  }
+
+  harvests = harvests.map(objKeysToCamel)
+  harvests = await Promise.all(harvests.map(expandHarvest))
+  harvests.sort((a, b) => { 
+    if (order === 'DESC') { 
+      return (a[sortColumn] <= b[sortColumn]) ? 1 : -1
+    } else {
+      return (a[sortColumn] >= b[sortColumn]) ? 1 : -1
+    }
+  })
+  
+  res.status(200).send(harvests)  
 })
 
+// helpers
+function convertGramsToPounds (grams) {
+  return grams / 453.59237
+}
+
+async function expandHarvest (harvest) {
+  const fullHarvest = { ...harvest }
+
+  const bays = await knex('bays').where({ id : fullHarvest.bay })
+  const bay = bays[0] // find one?
+
+  if (!bay) {
+    // raise exception?
+    return
+  }
+  // probably should have gone ahead and saved these in DB, lookup wants these too, could sort/order from DB
+  fullHarvest.harvestLbs = convertGramsToPounds(fullHarvest.harvestGrams)
+  fullHarvest.totalPlantLbs = convertGramsToPounds(fullHarvest.harvestLbs)
+  fullHarvest.percentHarvestedPlantWeight = (fullHarvest.harvestGrams / fullHarvest.totalPlantGrams)
+  fullHarvest.lbsHarvestedPerSqFt = (fullHarvest.harvestLbs / bay.square_footage)
+  fullHarvest.harvestLbsPerLight = (fullHarvest.harvestLbs / bay.light_count)
+  fullHarvest.sqFtPerPlant = (bay.square_footage / fullHarvest.plantCount)
+
+  return fullHarvest
+}
+
+function objKeysToCamel (obj) {
+  const fixed = {}
+  Object.keys(obj).forEach(k => {
+    fixed[camelCase(k)] = obj[k]
+  })
+  return fixed
+}
+
+function camelCase (str) {
+  return str.replace(/_([a-z])/g, g => g[1].toUpperCase());
+} 
+
+// server
 const server = app.listen(config.get('PORT'), () => {
   const port = server.address().port
   console.log(`App listening on port ${port}`)
